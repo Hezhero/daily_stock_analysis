4ed426fb506de7464e2d8f85fb90add38a4135ad
hzhero <851448443@qq.com>
2026-05-09 22:36:24 +0800

    优化回测脚本的日志打印

--- scripts/backtest_5y_22strategies.py
+++ scripts/backtest_5y_22strategies.py
@@ -74,7 +74,7 @@ class EmailConfig:
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
-    datefmt="%H:%M:%S",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 logger = logging.getLogger("backtest")
 
